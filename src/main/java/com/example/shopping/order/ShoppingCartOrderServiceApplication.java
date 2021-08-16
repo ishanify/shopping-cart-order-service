4da0f94b8180246a2dd53ad8aeae1d3246720ce2
Ishan <ishandeep999@gmail.com>
2021-08-16 01:16:02 -0400

    initial commit

---
+++ src/main/java/com/example/shopping/order/ShoppingCartOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.shopping.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCartOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingCartOrderServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/shopping/ShoppingCartOrderService1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartOrderService1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
