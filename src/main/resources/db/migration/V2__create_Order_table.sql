c5255758968ab31d58e6c4190b7b165cc9f09c66
Ishan <ishandeep999@gmail.com>
2021-08-22 21:44:48 -0400

    added WIP postman collection for order and product service

---
+++ src/main/resources/db/migration/V2__create_Order_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE ORDERS(
+id BIGINT NOT NULL auto_increment,
+user_id BIGINT NOT NULL,
+amount DECIMAL(10,2) NOT NULL,
+payment_status VARCHAR(50),
+created_date DATE,
+PRIMARY KEY (id),
+);	
---
+++ src/main/resources/db/migration/V3__insert_records.sql
@@ -0,0 +1,9 @@
+INSERT INTO CART (id, user_id, product_id, quantity, cart_product_status) VALUES (1, 100, 2, 4,'ACTIVE');
+INSERT INTO CART (id, user_id, product_id, quantity, cart_product_status) VALUES (2, 100, 1, 1,'ACTIVE');
+
+INSERT INTO CART (id, user_id, product_id, quantity, cart_product_status) VALUES (3, 200, 3, 1,'INACTIVE');
+INSERT INTO CART (id, user_id, product_id, quantity, cart_product_status) VALUES (4, 200, 4, 2,'INACTIVE');
+
+
+
+INSERT INTO ORDERS (id, user_id, amount, payment_status, created_date) VALUES (1, 200, 90, 4, 'SUCCESS', '2021-08-21');
